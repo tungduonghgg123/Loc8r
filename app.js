0375be4f228971ecf885f96c502de958760f53dd
tungduonghgg123 <tungduonghgg123@gmail.com>
2018-02-11 22:49:39 +0700

    Beginning of Chapter 7

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+// var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-// require('./app_server/models/db');
+require('./app_api/models/db');
 
 var routes = require('./app_server/routes/index');
 var users = require('./app_server/routes/users');
+
+// REST API
+var routesApi = require('./app_api/routes/index');
 
 var app = express();
 
@@ -15,7 +18,7 @@ var app = express();
 app.set('views', path.join(__dirname,'app_server', 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
+// app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded());
@@ -25,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/api', routesApi);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
